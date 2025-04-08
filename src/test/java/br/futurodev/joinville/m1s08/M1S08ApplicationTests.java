9d82d3a618278c42e5707fa34f871f8c2fa7bab1
Gabriel Augustin <gabriel.augustin_terceiros@nhecotech.com>
2025-04-07 21:07:47 -0300

    first commit

---
+++ src/test/java/br/futurodev/joinville/m1s08/M1S08ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.futurodev.joinville.m1s08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
